185cd8650584f6036b9e20eef01afa43987523b3
LAPTOP-E0NE7CPQ\Alexandra <smirnova.a.yu@gmail.com>
2018-02-28 09:55:17 +0300

    initial commit

---
+++ api_v0/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+
+
+class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Article.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ArticlePreviewSerializer
+        return ArticleDetailSerializer
